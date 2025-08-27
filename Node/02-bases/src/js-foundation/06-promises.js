81d3aab9a11f9b8e46718986a6c857ca07767001
Jonatan <jgutierreziglesias@gmail.com>
2025-08-27 13:09:46 +0200

    apartado adaptaciones terminado

--- Node/02-bases/src/js-foundation/06-promises.js
+++ Node/02-bases/src/js-foundation/06-promises.js
@@ -1,12 +1,14 @@
-
+const { http } = require('../plugins');
 
 const getPokemonById = async( id ) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon/${ id }`;
 
+    const pokemon = await http.get( url );
 
-    const response = await fetch( url );
-    const pokemon = await response.json();
+
+    //const response = await fetch( url );
+    //const pokemon = await response.json();
 
     //throw new Error( 'El pokemon no existe');
 
