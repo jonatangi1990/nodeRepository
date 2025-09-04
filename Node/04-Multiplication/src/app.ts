9a01e9bd347fd71bac322398b54a4f54d752657e
Jonatan <jgutierreziglesias@gmail.com>
2025-09-04 11:52:51 +0200

    finalizado multiplication

---
+++ Node/04-Multiplication/src/app.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+
+let outputMessage = "";
+const base = 5;
+const headerMessage = `
+==========================================
+        Tabla del ${ base }
+==========================================\n
+`;
+
+
+for( let i = 1; i <= 10; i++ ) {
+    outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
+}
+
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+
+const outputPath = `outputs/folder1/folder2/folder3`;
+
+
+
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`,outputMessage);
+console.log('File created!');
