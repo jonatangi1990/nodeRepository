8508ccb36f4a8d12685b1f6613ed1378f4332401
Jonatan <jgutierreziglesias@gmail.com>
2025-09-04 13:37:18 +0200

    terminado yargs

--- Node/04-Multiplication/src/app.ts
+++ Node/04-Multiplication/src/app.ts
@@ -1,9 +1,6 @@
+import { yarg } from "./config/plugins/args.plugin";
 
 
+//console.log(process.argv);
 
-console.log(process.argv);
-
-
-const [tsnode, app, ...args] = process.argv;
-
-console.log(args);
+console.log(yarg.b);
